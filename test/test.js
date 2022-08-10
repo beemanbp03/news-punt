2f21863fe047f94dc1e966b976a0ecfcdc5c727f
beemanbp03 <boulder.payne.beeman@gmail.com>
2022-08-10 15:28:15 -0500

    unit test functionality setup up using Mocha

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+const { isTypedArray } = require('util/types');
+describe('Array', () => {
+    describe('#indexOf()', () => {
+        it('should return -1 when the value is not present', () => {
+            assert.equal([1,2,3].indexOf(4), -1);
+        });
+    });
+});
