6c9863dc961bbc8261fa0e75d5afc7946e4b85de
beemanbp03 <boulder.payne.beeman@gmail.com>
2023-02-02 13:44:12 -0600

    added backticks whenever JWT_SECRET is being signed

--- controllers/authController.js
+++ controllers/authController.js
@@ -173,7 +173,7 @@ exports.login = async (req, res) => {
                 }else {
                     const id = loginResults[0].idUser;
                     //Create Token
-                    const tokenTraditional = jwt.sign({id: id}, process.env.JWT_SECRET, {
+                    const tokenTraditional = jwt.sign({id: id}, `${process.env.JWT_SECRET}`, {
                         expiresIn: process.env.JWT_EXPIRES_IN,
                     });
         
